668de2b727735a529473ead346a988d61b2f2be6
Stuart <stupop010@hotmail.co.uk>
2020-03-03 20:24:27 +0000

    create pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,7 @@ exports.createPages = ({ actions }) => {
   const { createPage } = actions
   createPage({
     path: "/explore/co-op-missions",
-    component: path.resolve("./src/components/CoOpMission/index.js"),
+    component: path.resolve("./src/templates/CoOpMissionTemplate.js"),
     context: {},
   })
 }
