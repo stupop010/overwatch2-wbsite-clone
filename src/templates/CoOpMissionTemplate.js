c8d50f2f68024e836994c7664be78d53eb90caae
Stuart <stupop010@hotmail.co.uk>
2020-03-03 20:30:36 +0000

    create pages

---
+++ src/templates/CoOpMissionTemplate.js
@@ -0,0 +1,11 @@
+import CoOpMission from "../components/CoOpMission"
+
+export default CoOpMission
+
+// import React from "react"
+
+// const CoOpMissionTemplate = () => {
+//   return <div>hello</div>
+// }
+
+// export default CoOpMissionTemplate
